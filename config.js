09cad6429b32198162ff4a3d7fbcf8a8cf447e04
wily <103911498+botreaction@users.noreply.github.com>
2024-06-24 16:16:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
 	name: 'Wily Kun', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
 	owner: ''6289688206739', // Ubah Nomor ke Nomor owner
 	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
-	faston: 100 // Semakin dikit nilai nya, maka semakin cepat read SW
+	faston: 10 // Semakin dikit nilai nya, maka semakin cepat read SW
 }
 export default config;
